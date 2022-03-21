7fe6671f95657aef0bcd090e11fd6127c43c5074
Dreamer <102036548+MXoooooo@users.noreply.github.com>
2022-03-21 21:44:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'eb788b7b2e81415f95a1f1c9cf8a36b0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'MX的博客',
+  domain: 'https://encouraging-homburg-f76.notion.site/MX-eb788b7b2e81415f95a1f1c9cf8a36b0',
+  author: 'MX',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  weibo: '不减到140不该名儿',
+  github: 'MXoooooo',
+  QQ: '1753913716',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
